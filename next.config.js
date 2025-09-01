ff485a0b9a4ad0973f3d636b3bacf535e2cca370
aragamix01 <aragamix01@gmail.com>
2025-09-01 17:07:36 +0700

    fix: next issues

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   images: {
     domains: ['raw.githubusercontent.com', 'assets.pokemon.com'],
     formats: ['image/webp', 'image/avif'], // Modern formats for optimization
-    minimumCacheTTL: 60 * 60 * 24 * 30, // Cache for 30 days
+    minimumCacheTTL: 60 * 60 * 24 * 30, // Cache for 30 days,
+    unoptimized: true
   },
 }
 
